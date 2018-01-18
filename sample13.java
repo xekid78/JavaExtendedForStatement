a7c576335b56f510fe5c4f9f762a8e1a47d46403
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 22:44:45 +0900

    拡張for文

---
+++ sample13.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class sample13 {
+
+	public static void main(String[] args) {
+		String[] team = {"佐藤","鈴木","田中","岸田","有森"};
+		for (String member : team) {
+			System.out.println(member + "さん");
+		}
+
+	}
+
+}
